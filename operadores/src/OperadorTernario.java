fd32611f32ced3d187f332885bc167dac62abfa5
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-09-14 14:08:18 -0300

    Criando exemplode condição com operador ternário

---
+++ operadores/src/OperadorTernario.java
@@ -0,0 +1,25 @@
+public class OperadorTernario {
+    public static void main(String[] args){
+
+       // classe Operadores.java
+      int a, b;
+
+      a = 5;
+      b = 6;
+      String resultado = "";
+
+     /* EXEMPLO DE CONDICIONAL UTILIZANDO OPERADOR TERNÁRIO */
+     resultado = a==b ? "verdadeiro" : "falso";
+      
+      System.out.println(resultado);
+
+      /* EXEMPLO DE CONDIÇÃO UTILIZANDO A ESTRUTURA IF/ELSE */
+      /*
+      if(a==b){
+        resultado = "verdadeiro"
+      } else {
+        resultado = "Falso"
+      }
+      */
+    }
+}
